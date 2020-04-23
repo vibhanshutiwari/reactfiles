947d8dae789fcbc81b5816a3bc32f70d0b690311
vibhanshutiwari <vibhanshtiwari1999@gmail.com>
2020-04-23 21:36:09 +0530

    list and key

--- src/events.js
+++ src/events.js
@@ -1,46 +1,32 @@
 
 import React from 'react';
 
-class Hellogeeks extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            inlogged: true
-        }
-    }
-      render() {
-      
-          // First method //
-  
-         // if(this.state.inlogged) {
-           //return   <div> Conditional rendering function</div>
-          //} else {
-             //return <div>function not working.</div>
-          //}
-          
-          // Second method
-  
-          //let message
-          //if(this.state.inlogged) {
-            //  message = <div> Conditional rendering function</div>
-          //} else {
-            //  message = <div>function not working.</div>
-          //}
-             //return <div>{message}</div>
-  
-          // Third method //
-  
-            //return this.state.inlogged ? <div> Conditional rendering function</div>
-             //: <div>function not working.</div>
-  
-            // Four method
-  
-             return this.state.inlogged && <div class="container bg-danger"> 
-             <p>if first condition is right then code works.</p>
-  
-             </div>
-      }
-  }
-  
 
-export default Hellogeeks;
+function Result() {
+  
+  const person = [{
+    id : 1,
+    name: 'shubham',
+    job: 'teaher',
+  },
+  {
+    id: 2,
+    name: 'aman',
+    job: 'accounted',
+  },
+  {
+   id: 3,
+   name: 'manish',
+   job: 'watchman',
+  }
+];
+ const personresult = person.map(value => (
+  <p key={value.id} >  {value.name} {value.job} </p>
+  ));
+  
+  return <div className="container"> {personresult}</div>
+}
+
+export default Result;
+
+
