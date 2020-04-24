77b280361f64666795bb22637deb300ce7da0554
vibhanshutiwari <vibhanshtiwari1999@gmail.com>
2020-04-24 10:14:47 +0530

    Fragment

--- src/App.js
+++ src/App.js
@@ -1,11 +1,54 @@
 import React from 'react';
-
 import './App.css';
 
-// Lifecycle Methods ..
 
+class App extends React.Component {
+    render() {
+        return(
+            <div>
+             <Tables />
+             <Show />
+            </div>
+        );
+    }
+}
 
-    
+  function Show() {
+   const styles = {
+       color: 'red',
+     };
+
+    return (
+        <React.Fragment>
+          <h4 style = {styles} >FragmentDemo</h4>
+          <p>This describe the fragment demo Component.</p>
+          </React.Fragment>
+    );
+  }
+
+  function Tables() {
+    return (
+      <table>
+        <tbody>
+          <tr>
+        <Column />
+          </tr>
+        </tbody>
+      </table>
+    );
+  }
+  
+  
+    function Column() {
+      return (
+      <>
+        <td>Name</td>
+        <td>Shubham</td>
+      </>
+      );
+    }
+
+    export default App;
 
  
 
