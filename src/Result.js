96c247a55fd576e5985cd7b4f5342c6eec3c8023
vibhanshutiwari <vibhanshtiwari1999@gmail.com>
2020-04-26 22:28:35 +0530

    Add the Value every one Sec

--- src/Result.js
+++ src/Result.js
@@ -2,54 +2,28 @@ import React, { useState, useEffect } from 'react';
 
 
 
-// Conditionally run effect //
-
-//function CounterHookone() {
-  //  const [name, setname] = useState(0);
-   // const [count, setcount] = useState('');
-
-   // useEffect(() => {
-     //   console.log('useEffect -- updateing document title')
-       // document.title = `You clicked ${name} timmes.`
-   // },[name]);
-
-   // return (
-     //   <div>
-       //     <input type="text" value={count}
-         //       onChange={e => setcount(e.target.value)} />
-           // <button onClick={() => setname(name + 1)}>click {name}</button>
-       // </div>
-   // );
-// }
-
-// export default CounterHookone;
 
 
-function HookMouse () {
-  const [x, setX] = useState(0);
-  const [y, setY] = useState(0);
+function HookMouse() {
+  const [count, setCount] = useState(0);
 
-  const logMousePosition = e => {
-      console.log('mouseevents')
-      setX(e.clientX)
-      setY(e.clientY)
+  const countTime = () => {
+    setCount(preveCount => preveCount + 1);
   }
 
-useEffect (() => {
-    console.log('useEffect called');
-    window.addEventListener('mousemove', logMousePosition );
+  useEffect(() => {
+    const Interval = setInterval(countTime, 1000);
 
- return () => {
-   console.log('Component unMounting code');
-   window.removeEventListener('mousemove', logMousePosition);
- }
-}, []);
+    return () => {
+      clearInterval(Interval);
+    }
+  }, []);
 
-    return (
-         <div className="container">
-       Hooks X- {x} Y- {y}
-         </div>
-    );
+  return (
+    <div className="container">
+      {count}
+    </div>
+  );
 }
 
 export default HookMouse;
