be6d73b9032867070f9fe50fed634de90761ceb4
vibhanshutiwari <vibhanshtiwari1999@gmail.com>
2020-04-27 22:19:15 +0530

    Ref Hook

--- src/events.js
+++ src/events.js
@@ -1,33 +1,42 @@
 
-import React, { useState, useMemo } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
-// Callback with useState //
 
-function CallBacks() {
-    const [age, setAge] = useState(20);
-    const [salary, setSalary] = useState(20000);
+//function UseRefs() {
+  //  const inputRef = useRef(null);
 
-  const ageHandler = () => {
-        setAge(age + 1);
-    }
+    //useEffect(() => {
+      //  inputRef.current.focus();
+    //}, []);
 
-   const salaryHandler = () => {
-        setSalary(salary + 2000);
-    }
+    //return (
+      //  <input type="text" ref={inputRef} />
+  //  );
+//}
 
-const isEven = useMemo(() => {
-    let i = 0;
-    while( i < 200000) i++
-    return age % 2 === 0
-  }, [age]);
+
+//export default UseRefs;
+
+function HookTimer () {
+    const [timer, setTimer] = useState(0);
+      const InteralHook = useRef();
+
+   useEffect(()=>{
+    InteralHook.current = setInterval(() => {
+     setTimer(prevTimer => prevTimer + 1 );  
+  }, 1000);
+  return () => {
+      clearInterval(InteralHook.current);
+  };
+   }, []);
 
     return (
-        <div className="container">
-            <span> Show Even and Odd .. {isEven ? 'even' : 'odd'}</span>
-            <button onClick={ageHandler}>Increment Age {age}</button>
-            <button onClick={salaryHandler}>Increment Salary {salary}</button>
+        <div>
+        CountTimer-- {timer}
+        <button onClick={() => clearInterval(InteralHook.current)}>click to stop function</button>
         </div>
     );
 }
 
-export default CallBacks;
+
+export default HookTimer;
